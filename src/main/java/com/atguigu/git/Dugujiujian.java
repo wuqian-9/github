56ea6e4b9917cb3ba64604fe5f7f21e78650a624
wuqian-9 <71486909+wuqian-9@users.noreply.github.com>
2020-09-18 20:30:00 +0800

    Update Dugujiujian.java

--- src/main/java/com/atguigu/git/Dugujiujian.java
+++ src/main/java/com/atguigu/git/Dugujiujian.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class Dugujiujian {
     public static void main(String[] args) {
         System.out.println("===============");
-        System.out.println("你看着有没");
+        System.out.println("你看着有没有");
         System.out.println("===============");
     }
 }
